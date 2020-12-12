34700b8bbb4352043aedef80c686390926f1bf2b
Akshay Nair <phenax5@gmail.com>
2020-12-12 11:35:15 +0530

    refactors modules initialization

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,19 +1,18 @@
-local theme = require 'modules.theme'
-local buffers = require 'modules.buffers'
-local fs = require 'modules.fs'
-local coc = require 'modules.coc'
-local git = require 'modules.git'
+local modules = {
+  require 'modules.theme',
+  require 'modules.buffers',
+  require 'modules.fs',
+  require 'modules.coc',
+  require 'modules.git',
+  require 'modules.ide',
+}
 
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
-  -- Packer
-  use {'wbthomason/packer.nvim', opt = true}
+  use {'wbthomason/packer.nvim', opt = true} -- Packer
 
-  -- Theme
-  theme.init(use)
-  buffers.init(use)
-  fs.init(use)
-  coc.init(use)
-  git.init(use)
+  for _, m in pairs(modules) do
+    m.init(use)
+  end
 end)
 
