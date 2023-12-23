23b860cfe95cffc6205d7e1d1e971ed13c4671f6
Akshay Nair <phenax5@gmail.com>
2023-12-24 00:29:29 +0530

    feat: treesj plugin

--- lua/_plugins/autopairs.lua
+++ lua/_plugins/autopairs.lua
@@ -10,7 +10,12 @@ return {
     }
 
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+    require('cmp').event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done({
+        map_char = { tex = '', },
+      })
+    )
   end,
 }
 
---
+++ lua/_plugins/treesj.lua
@@ -0,0 +1,18 @@
+local plugin = {
+  'Wansmer/treesj',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+
+  keys = {
+    { mode = 'n', '<leader>tt', ':TSJToggle<cr>' },
+    { mode = 'n', '<leader>ts', ':TSJSplit<cr>' },
+    { mode = 'n', '<leader>tj', ':TSJJoin<cr>' },
+  },
+}
+function plugin.config()
+  require('treesj').setup({
+    use_default_keymaps = false,
+    max_join_length = 200,
+  })
+end
+
+return plugin
