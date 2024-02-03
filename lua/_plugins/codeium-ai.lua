c22401c98ed9af01f53caa7b679d733535d5753c
Akshay Nair <phenax5@gmail.com>
2024-02-03 18:32:08 +0530

    feat: adds lsp_lines

--- lua/_plugins/codeium-ai.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'Exafunction/codeium.vim',
-  enabled = false,
-
-  config = function()
-    vim.g.codeium_enabled = false
-    vim.api.nvim_set_keymap('i', '<c-g><c-g>', 'codeium#Accept()', { silent = true, expr = true })
-  end,
-}
---
+++ lua/_plugins/lsp-lines.lua
@@ -0,0 +1,11 @@
+return {
+  'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  event = 'VeryLazy',
+  keys = {
+    { '<leader>dv', function() require'lsp_lines'.toggle() end, mode = 'n' },
+  },
+  config = function()
+    require'lsp_lines'.setup {}
+    vim.diagnostic.config({ virtual_lines = false })
+  end,
+}
