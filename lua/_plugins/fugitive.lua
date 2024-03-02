7b6979fda3a073f26cb932511712ca9ce432f047
Akshay Nair <phenax5@gmail.com>
2024-03-02 19:28:59 +0530

    feat: adds diffview keys

--- lua/_plugins/fugitive.lua
+++ lua/_plugins/fugitive.lua
@@ -1,12 +1,16 @@
 return {
   'tpope/vim-fugitive',
   keys = {
-    { mode = 'n', '<localleader>gs', ':G<cr>' },
-    { mode = 'n', '<localleader>gaf', ':Git add %<cr>' },
-    { mode = 'n', '<localleader>gcc', ':Git commit<cr>' },
-    { mode = 'n', '<localleader>gca', ':Git commit --amend<cr>' },
-    { mode = 'n', '<localleader>gpp', ':Git push<cr>' },
-    { mode = 'n', '<localleader>gpu', ':Git pull<cr>' },
+    { mode = 'n', '<localleader>gs', '<cmd>G<cr>' },
+    { mode = 'n', '<localleader>gaf', '<cmd>Git add %<cr>' },
+    { mode = 'n', '<localleader>gcc', '<cmd>Git commit<cr>' },
+    { mode = 'n', '<localleader>gca', '<cmd>Git commit --amend<cr>' },
+    { mode = 'n', '<localleader>gpp', '<cmd>Git push<cr>' },
+    { mode = 'n', '<localleader>gpu', '<cmd>Git pull<cr>' },
+
+    -- Diffresult merge in left/right (Technically not fugitive but its fine)
+    { mode = 'n', '<leader>gl', '<cmd>diffget //2<cr>' },
+    { mode = 'n', '<leader>gr', '<cmd>diffget //3<cr>' },
   },
   cmd = { 'Git', 'G' },
 }
